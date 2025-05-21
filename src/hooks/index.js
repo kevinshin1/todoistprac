b28da275f00a99292cc7a51e0ba9e38b01442448
kevinshin1 <kshinkimchi@gmail.com>
2025-05-20 22:19:35 -0400

    Added usestate and useeffect hooks

---
+++ src/hooks/index.js
@@ -0,0 +1,21 @@
+import { useState, useEffect } from 'react';
+import { firebase } from '../firebase';
+
+const collatedTasksExist = () => {};
+
+export const useTasks = selectedProject => {
+    const [tasks, setTasks] = useState([]);
+    
+    useEffect(() => {
+        let unsubscribe = firebase.firestore().collection('tasks')
+            .where('userId', '==', 'kjs1');
+
+            // Gets the tasks from the selected project depending on each condition
+            unsubscribe = selectedProject && !collatedTasksExist(selectedProject) ? (unsubscribe = unsubscribe.where('projectId', '==', selectedProject))
+            : selectedProject === 'TODAY'
+            ? (unsubscribe = unsubscribe.where('date', '==', moment().format('DD/MM/YYYY')))
+            : selectedProject === 'INBOX' || selectedProject === 0
+            ? (unsubscribe = unsubscribe.where('date', '==', ''))
+            : unsubscribe;
+    }, [selectedProject]);
+}
