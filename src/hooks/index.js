91530f25d0454c7c38459bf9c8bbe2f9e412ac3c
kevinshin1 <kshinkimchi@gmail.com>
2025-05-21 22:26:34 -0400

    Added useTasks and useProjects hooks for project retrieval, changed firebase and node sass versions

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,10 +1,12 @@
 import { useState, useEffect } from 'react';
 import { firebase } from '../firebase';
-
-const collatedTasksExist = () => {};
+import { collatedTasksExist } from '../helpers';
+import moment from 'moment';
+import { doc } from 'firebase/firestore';
 
 export const useTasks = selectedProject => {
     const [tasks, setTasks] = useState([]);
+    const [archivedTasks, setArchivedTasks] = useState([]);
     
     useEffect(() => {
         let unsubscribe = firebase.firestore().collection('tasks')
@@ -17,5 +19,48 @@ export const useTasks = selectedProject => {
             : selectedProject === 'INBOX' || selectedProject === 0
             ? (unsubscribe = unsubscribe.where('date', '==', ''))
             : unsubscribe;
+
+            unsubscribe = unsubscribe.onSnapshot(snapshot => {
+                const newTasks = snapshot.docs.map(task => ({
+                    id: task.id,
+                    ...task.data(),
+                }));
+
+                setTasks(
+                    selectedProject === 'NEXT_7'
+                    ? newTasks.filter(task => moment(task.date, 'DD/MM/YYYY').diff(moment(), 'days') <= 7 && task.archived !== true)
+                    : newTasks.filter(task => task.archived !== true)
+                );
+
+                setArchivedTasks(newTasks.filter(task => task.archived !== false));
+            });
+
+            return () => unsubscribe();
     }, [selectedProject]);
-}
+
+    return { tasks, archivedTasks };
+};
+
+export const useProjects = () => {
+    const [projects, setProjects] = useState([]);
+
+    useEffect(() => {
+        firebase.firestore().collection('projects').where('userId', '==', 'kjs1')
+        .orderBy('projectId')
+        .get()
+        .then(snapshot => {
+            const allProjects = snapshot.docs.map(project => ({
+                ...project.data(),
+                docId: project.id,
+            }));
+
+            // Checks if the projects are already in the state, if not, sets the projects in the state
+            // This prevents the infinite loop of useEffect
+            if (JSON.stringify(allProjects) !== JSON.stringify(projects)) {
+                setProjects(allProjects);
+            }
+        });
+    }, [projects]);
+
+    return { projects, setProjects };
+};
