acc9afa1e6ce99c3e10705a57563a478aaaf82d2
kevinshin1 <kshinkimchi@gmail.com>
2025-05-22 22:59:57 -0400

    Built the tasks and checkbox components, fixed firebase imports

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,5 @@
-import firebase from 'firebase/app';
-import 'firebase/firestore';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
 
 const firebaseConfig = firebase.initializeApp({
     apiKey: "AIzaSyBGE1RZAffQR4OvjhsxtuvdTbNg-dzWbZw",
