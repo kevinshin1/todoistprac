35c2bcd75c16e58b7172aeb6d618548721568a3b
kevinshin1 <kshinkimchi@gmail.com>
2025-06-05 21:57:03 -0400

    Added tasks to stylesheet

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -32,7 +32,7 @@ export const Tasks = () => {
         <div className="tasks" data-testid="tasks" >
             <h2 data-testid="project-name">{projectName}</h2>
 
-            <ul className="task__list">
+            <ul className="tasks__list">
                 {tasks.map(task=> (
                     <li key={`${task.id}`}>
                         <Checkbox id={task.id} />
